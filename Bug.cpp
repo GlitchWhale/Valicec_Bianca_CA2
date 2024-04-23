4e7529db9264014a8d44b313aad44f97782962f0
Bianca Valicec <biavalicec@gmail.com>
2024-04-23 20:30:02 +0100

    fixed the bug method isWayBlocked to handle the board size of 0 to 9. Added bugs text file to read from

--- Bug.cpp
+++ Bug.cpp
@@ -4,9 +4,9 @@
 bool Bug::isWayBlocked(Direction dir) {
     if (dir== North && position.second == 0) {
         return true;
-    } else if (dir == East && position.first == size - 1) {
+    } else if (dir == East && position.first == 9) {
         return true;
-    } else if (dir == South && position.second == size - 1) {
+    } else if (dir == South && position.second == 9) {
         return true;
     } else if (dir == West && position.first == 0) {
         return true;
