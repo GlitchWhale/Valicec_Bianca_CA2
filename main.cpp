b0457ffa9ab134cde71424cfbb8274aede415c20
Bianca Valicec <biavalicec@gmail.com>
2024-04-23 20:40:26 +0100

    created readfile function in main, created the bug vector and started the parseline function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "Bug.h"
+
+using namespace std;
+
+void readFromFile();
+void parseLine(const string& strline);
 
 int main() {
+    vector<Bug*> bugs_vector;
+}
 
+void readFromFile(){
+    ifstream inFileStream("bugs.txt");
+
+    if(inFileStream.good()){
+        string line;
+
+        while(getline(inFileStream, line)){
+            parseLine(line);
+        }
+
+        inFileStream.close();
+    }
+    else{
+        cout << "Error opening file" << endl;
+    }
+}
+
+void parseLine(const string& strline){
+    stringstream strSteam(strline);
+
+    const char delim = ';';
+    string type;
 }
